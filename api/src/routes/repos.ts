c9478480a5b5abe5de68911dff863be27bb02eec
Adam Bowman <adamcbowman@gmail.com>
2022-05-07 11:32:23 -0300

    get repos from api/data/repos.json

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-shadow */
 import { Router, Request, Response } from 'express';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  //return arrays from data/repos.json where fork = false
+
+  const repos = require('../../data/repos.json');
+  const reposData = repos.filter(
+    (repo: { fork: boolean }) => repo.fork === false
+  );
+  res.json(reposData);
 });
